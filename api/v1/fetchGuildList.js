be9078294636513e35ae51d849921edf4b27048d
Lucas <36572374+DevChromium@users.noreply.github.com>
2019-10-01 16:00:04 +0200

    Update fetchGuildList.js

--- api/v1/fetchGuildList.js
+++ api/v1/fetchGuildList.js
@@ -1,1 +1,15 @@
-const axios = require("avios"); 
+const axios = require("axios"); 
+
+module.exports = async function fetchGuildList() {
+    return new Promise((resolve, reject) => {
+      let endpoint = `https://api.wynncraft.com/public_api.php?action=guildList`
+            req.get(endpoint, (err, res, body) => {
+                if (!err && res.statusCode == '200') {
+                    return resolve(JSON.parse(body));
+                } else {
+                    return reject(JSON.parse(body));
+                }
+            });   
+    });
+  
+} 
